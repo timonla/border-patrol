7ff696292913826eae7c4d20b9a6c2620dd00b16
Timon Lapawczyk <timon.lapawczyk@gmail.com>
2020-10-29 18:19:20 +0100

    Add scripts and remove BuildFiles from git

---
+++ BorderPatrol/Model.Script.cs
@@ -0,0 +1,22 @@
+//--------------------------------------------------------------
+// Press F1 to get help about using script.
+// To access an object that is not located in the current class, start the call with Globals.
+// When using events and timers be cautious not to generate memoryleaks,
+// please see the help for more information.
+//---------------------------------------------------------------
+
+namespace Neo.ApplicationFramework.Generated
+{
+    using System.Windows.Forms;
+    using System;
+    using System.Drawing;
+    using Neo.ApplicationFramework.Tools;
+    using Neo.ApplicationFramework.Common.Graphics.Logic;
+    using Neo.ApplicationFramework.Controls;
+    using Neo.ApplicationFramework.Interfaces;
+    
+    
+    public partial class Model
+    {
+    }
+}
